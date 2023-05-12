c781e1534bb2b126ddfa7d3645a52f6fe718e90c
GURitch <guritch21@gmail.com>
2023-05-12 12:45:30 +0300

    test

---
+++ src/main/java/skypro/javaind/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package skypro.javaind.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+}
